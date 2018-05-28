92f676ee428420e019a8c48de5ef9c79e9e74d97
Fathima-N <aadhilah.nizamdeen@gmail.com>
2018-05-28 17:09:26 +0000

    got hardcoded reviews to display per product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,6 +6,13 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find params[:id]
+    @reviews = Review.select {|item| item.product_id.to_i == params[:id].to_i }
+    	#DEBUGGING TO SEE IF REVIEW ID MATCHES PARAMS ID
+    	# puts item.product_id
+    	# puts params[:id].to_i
+    	# puts item.product_id == params[:id].to_i
+    
+    # @reviews = Review.find params[:id]
   end
 
 end
---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,6 @@
+class ReviewsController < ApplicationController
+  def show
+  	# @reviews = Review.find params[:id]
+  	# @reviews = Review.all
+  end
+end
